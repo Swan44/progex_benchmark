6d60612a504d4e6857641cad8c319a1500d0230b
Swan44 <echodsj44@163.com>
2025-08-17 21:50:11 +0800

    util

---
+++ mutant_programs/WordUtilsCapitalize/mutants/mutant_086/WordUtilsCapitalize.java
@@ -0,0 +1,27 @@
+public class WordUtilsCapitalize {
+    public static String capitalize( String str, char[] delimiters )
+    {
+        int delimLen = delimiters == null ? -1 : delimiters.length;
+        if (str == null || str.length() == 0 || delimLen == 0) {
+            return str;
+        }
+        int strLen = str.length();
+        StringBuffer buffer = new StringBuffer(strLen);
+        boolean capitalizeNext = true;
+        for (int i = 0; i <= strLen; i++) {
+            char ch = str.charAt(i);
+            if (isDelimiter(ch, delimiters)) {
+                buffer.append(ch);
+                capitalizeNext = true;
+            } else {
+                if (capitalizeNext) {
+                    buffer.append(Character.toTitleCase( ch ));
+                    capitalizeNext = false;
+                } else {
+                    buffer.append(ch);
+                }
+            }
+        }
+        return buffer.toString();
+    }
+}
