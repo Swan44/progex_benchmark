ae008a8acdfbc7f379dc4dad8e30aef2ec7d0e0e
Swan44 <echodsj44@163.com>
2025-08-22 02:17:49 +0800

    util

--- src/main/java/util/MutantClassifier.java
+++ src/main/java/util/MutantClassifier.java
@@ -13,14 +13,31 @@ import java.util.Map;
 
 public class MutantClassifier {
 
-    private static final Map<String, int[]> CLASSIFICATION_RULES = Map.of(
-            "D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsDiv\\QuickSortQuickmutants.json", new int[]{7, 33},
-            "D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsDiv\\QuickSortSortmutants.json", new int[]{35, 48},
-            "D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsDiv\\QuickSortSwapmutants.json", new int[]{50, 55}
+    private static final Map<String, int[]> CLASSIFICATION_RULES = Map.ofEntries(
+            Map.entry("D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsDiv\\ArrayUtilsLastBooleanmutants.json", new int[]{2238, 2253}),
+            Map.entry("D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsDiv\\ArrayUtilsLastBytemutants.json", new int[]{1806, 1821}),
+            Map.entry("D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsDiv\\ArrayUtilsLastCharmutants.json", new int[]{1702, 1717}),
+            Map.entry("D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsDiv\\ArrayUtilsLastDoublemutants.json", new int[]{1977, 1992}),
+            Map.entry("D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsDiv\\ArrayUtilsLastDoubleTolemutants.json", new int[]{2011, 2028}),
+            Map.entry("D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsDiv\\ArrayUtilsLastFloatmutants.json", new int[]{2133, 2148}),
+            Map.entry("D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsDiv\\ArrayUtilsLastIntmutants.json", new int[]{1492, 1507}),
+            Map.entry("D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsDiv\\ArrayUtilsLastLongmutants.json", new int[]{1389, 1404}),
+            Map.entry("D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsDiv\\ArrayUtilsLastObjmutants.json", new int[]{1278, 1301}),
+            Map.entry("D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsDiv\\ArrayUtilsLastShortmutants.json", new int[]{1595, 1610}),
+            Map.entry("D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsDiv\\ArrayUtilsSubBooleanmutants.json", new int[]{753, 771}),
+            Map.entry("D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsDiv\\ArrayUtilsSubBytemutants.json", new int[]{636, 654}),
+            Map.entry("D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsDiv\\ArrayUtilsSubCharmutants.json", new int[]{597, 615}),
+            Map.entry("D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsDiv\\ArrayUtilsSubDoublemutants.json", new int[]{675, 693}),
+            Map.entry("D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsDiv\\ArrayUtilsSubFloatmutants.json", new int[]{714, 732}),
+            Map.entry("D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsDiv\\ArrayUtilsSubIntmutants.json", new int[]{519, 537}),
+            Map.entry("D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsDiv\\ArrayUtilsSubLongmutants.json", new int[]{480, 498}),
+            Map.entry("D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsDiv\\ArrayUtilsSubObjmutants.json", new int[]{441, 459}),
+            Map.entry("D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsDiv\\ArrayUtilsSubShortmutants.json", new int[]{558, 576}),
+            Map.entry("D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsDiv\\ArrayUtilsToMapmutants.json", new int[]{236, 261})
     );
 
     public static void main(String[] args) {
-        String inputFile = "D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsIDJson\\QuickSortmutants.json";
+        String inputFile = "D:\\bishe_code\\progex_benchmark\\mutantbench\\mutantjava\\mutantsIDJson\\ArrayUtilsmutants.json";
         try {
             classifyMutants(inputFile);
         } catch (IOException e) {
